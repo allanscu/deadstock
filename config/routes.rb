5890ada9218057be7befcc78c3bb5298378a65e5
allanscu <ateruel@gmail.com>
2018-02-15 13:16:26 -0800

    added author module

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  get 'about' => 'pages#about', as: :about
-  get 'contact' => 'pages#contact', as: :contact
-  get 'posts' => 'posts#index', as: :posts
-  get 'posts/:id' => 'posts#show', as: :post
-  root to: 'posts#index'
-  resources :posts
+
+  root to: 'blog/posts#index'
+  
+  namespace :author do
+    resources :posts
+  end
+  
+  scope module: 'blog' do 
+    get 'about' => 'pages#about', as: :about
+    get 'contact' => 'pages#contact', as: :contact
+    get 'posts' => 'posts#index', as: :posts
+    get 'posts/:id' => 'posts#show', as: :post
+  end
+  
   resources :releases
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
