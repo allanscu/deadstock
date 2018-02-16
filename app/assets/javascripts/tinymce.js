2be0e1027e8ca6c51b6fb9405ca956635280a0a9
allanscu <ateruel@gmail.com>
2018-02-16 15:59:00 -0800

    cleanup tiny

--- app/assets/javascripts/tinymce.js
+++ app/assets/javascripts/tinymce.js
@@ -2,7 +2,7 @@ $(document).on("turbolinks:load", function () {
   if (typeof tinyMCE != 'undefined') {
 	tinyMCE.remove();
     tinyMCE.init({
-		selector:'textarea#post_body',
+		selector: "textarea.tinymce",
 		plugins: "codesample image media link code ",
 		toolbar: "undo redo | styleselect | bold italic link | codesample image media | code"
     });
