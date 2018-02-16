faadd5ce94c0ed2a1baa4c74b059541938a590b2
allanscu <ateruel@gmail.com>
2018-02-16 13:44:12 -0800

    added tiny js directly to view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,11 +17,3 @@
 //= require turbolinks
 //= require_tree .
 
-document.addEventListener("turbolinks:load", function() {
-	tinymce.remove();
-	tinymce.init({
-		selector:'textarea#post_body',
-		plugins: "codesample image media link code ",
-		toolbar: "undo redo | styleselect | bold italic link | codesample image media | code"
-	});
-})
