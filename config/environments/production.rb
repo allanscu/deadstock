270549e70dbe59efba8a9ce6d9a6f24c152d721a
allanscu <ateruel@gmail.com>
2018-02-16 14:28:31 -0800

    fixing tiny

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -88,8 +89,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
-  
-  # Tinymce
-  config.assets.precompile = true
 end
