b8439778e4c668df0a2d46ab579056cd0df0d29d
allanscu <ateruel@gmail.com>
2018-02-16 16:34:00 -0800

    fixed turbolink issue

--- app/assets/javascripts/tinymce.js
+++ app/assets/javascripts/tinymce.js
@@ -1,8 +1,12 @@
-document.addEventListener("turbolinks:load", function() {
-	tinymce.remove();
-	tinymce.init({
+$(document).on('ready page:load', function () {
+  if (typeof tinyMCE != 'undefined') {
+	tinyMCE.remove();
+    tinyMCE.init({
 		selector:'textarea#post_body',
 		plugins: "codesample image media link code ",
 		toolbar: "undo redo | styleselect | bold italic link | codesample image media | code"
-	});
+    });
+  } else {
+    setTimeout(arguments.callee, 50);
+  }
 });
