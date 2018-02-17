9479071ee56457dc6a763eb5ed3623b3a961c9ae
allanscu <ateruel@gmail.com>
2018-02-16 16:06:31 -0800

    cleanup tiny

--- app/assets/javascripts/tinymce.js
+++ app/assets/javascripts/tinymce.js
@@ -1,10 +1,13 @@
-$(document).on("turbolinks:load", function () {
+document.addEventListener("turbolinks:load", function() {
   if (typeof tinyMCE != 'undefined') {
 	tinyMCE.remove();
     tinyMCE.init({
-		selector: "textarea.tinymce",
-		plugins: "codesample image media link code ",
-		toolbar: "undo redo | styleselect | bold italic link | codesample image media | code"
+      selector: "textarea.tinymce",
+      toolbar: [
+        "styleselect | bold italic | alignleft aligncenter alignright alignjustify",
+        "bullist numlist outdent indent | link image | code | codesample"
+      ],
+      plugins: "image,link,code,codesample,autoresize,imagetools,media,table,insertdatetime,charmap,print,preview,anchor,searchreplace,visualblocks,fullscreen"
     });
   } else {
     setTimeout(arguments.callee, 50);
